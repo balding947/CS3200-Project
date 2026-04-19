3ffbb842862f8b3272860530c9f1cf7ce93ff90a
Oscar Peng <peng.zheng@northeastern.edu>
2026-04-19 17:53:55 -0400

    Changed about section

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -9,10 +9,12 @@ st.write("# About this App")
 
 st.markdown(
     """
-    This is a demo app for Data and Software in International Government and Politics Dialogue 2025 Project Course.  
+    This is a demo financial management app for college students built for a database course.  
 
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
+    The goal of this app is to showcase features such as shared expense tracking, club budget management, 
+    and viewing financial analytics through a dashboard. It demonstrates how users can log expenses, 
+    split costs, manage categories, and monitor payments, while also supporting administrators with summarized 
+    insights and reporting tools.
 
     Stay tuned for more information and features to come!
     """
