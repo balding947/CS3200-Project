e22c0dc8a2e04764180113db2e97efefea280067
sharonwilfred <wilfred.s@northeastern.edu>
2026-04-19 16:37:50 -0400

    Update 20_Sofia_Home.py

--- app/src/pages/20_Sofia_Home.py
+++ app/src/pages/20_Sofia_Home.py
@@ -1,6 +1,5 @@
 import logging
 logger = logging.getLogger(__name__)
-
 import streamlit as st
 from modules.nav import SideBarLinks
 
@@ -8,3 +7,16 @@ st.set_page_config(layout='wide')
 SideBarLinks()
 
 st.title(f"Welcome, {st.session_state['first_name']}!")
+st.write('### What would you like to do today?')
+
+if st.button('Manage Categories',
+             type='primary', use_container_width=True):
+    st.switch_page('pages/21_Manage_Categories.py')
+
+if st.button('Review Flagged Transactions',
+             type='primary', use_container_width=True):
+    st.switch_page('pages/22_Flagged_Transactions.py')
+
+if st.button('View Support Issues',
+             type='primary', use_container_width=True):
+    st.switch_page('pages/23_Support_Issues.py')
